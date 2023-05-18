c1502d5f27d640757f8fecd268e0161712206104
Hamza Sahbaz <mailadresim@hotmail.com>
2023-05-18 22:08:15 +0100

    Prototype vs Singleton Bean scope

--- Spring-05-BeanScope/src/main/java/com/StartupApp.java
+++ Spring-05-BeanScope/src/main/java/com/StartupApp.java
@@ -14,8 +14,16 @@ public class StartupApp {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
+        //By default SPring use singleton Bean scope
         CommentService commentService = context.getBean(CommentService.class);
+        CommentService commentService2 = context.getBean(CommentService.class);
 
-        commentService.publishComment(comment);
+        System.out.println(commentService);
+        System.out.println(commentService2);
+
+        //with default Singleton Bean scope below statement will be true
+        //Whenever @Scope("prototype") added in CommentService class then result will be false
+        //because with @Scope annotation Spring create new object for each call
+        System.out.println(commentService2==commentService);
     }
 }
--- Spring-05-BeanScope/src/main/java/com/startup/service/CommentService.java
+++ Spring-05-BeanScope/src/main/java/com/startup/service/CommentService.java
@@ -4,9 +4,16 @@ import com.startup.model.*;
 import com.startup.proxy.*;
 import com.startup.repository.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.config.*;
+import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.*;
 
 @Component
+//Whenever somebody requested bean for CommentService, Spring will create
+//separate bean in the container
+//@Scope("prototype")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
 
     private final CommentRepository commentRepository;
