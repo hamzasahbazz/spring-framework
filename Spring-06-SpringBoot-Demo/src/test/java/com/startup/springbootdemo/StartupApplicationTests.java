c6e28f5a6c3210756c45eb00249a796b6dd7ed9c
Hamza Sahbaz <mailadresim@hotmail.com>
2023-05-20 18:21:07 +0100

    Spring boot project creation

---
+++ Spring-06-SpringBoot-Demo/src/test/java/com/startup/springbootdemo/StartupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
